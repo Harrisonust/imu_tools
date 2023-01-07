3e3bb0570859cf5bb65b2a76f5644b0ca30b8105
Harrisonust <henhen.luo@gmail.com>
2023-01-07 13:33:47 +0000

    rename

--- imu_filter_madgwick/launch/imu_cluster.launch.py
+++ imu_filter_madgwick/launch/imu_cluster.launch.py
@@ -12,30 +12,28 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource(
                     os.path.join(
                         get_package_share_directory('imu_filter_madgwick'),
-                        'launch/imu_filter_madgwick_imu0.launch.py'))
+                        'launch/imu0.launch.py'))
             ), 
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         os.path.join(
+            #             get_package_share_directory('imu_filter_madgwick'),
+            #             'launch/imu1.launch.py'))
+            # ), 
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(
                         get_package_share_directory('imu_filter_madgwick'),
-                        'launch/imu_filter_madgwick_imu1.launch.py'))
-            ), 
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory('imu_filter_madgwick'),
-                        'launch/imu_filter_madgwick_imu2.launch.py'))
+                        'launch/imu2.launch.py'))
             ),
             Node(
                 package='tf2_ros',
                 executable='static_transform_publisher',
-                name='tf2_ros0', 
                 arguments=['1', '1', '1', '0', '0', '0', 'palm', 'imu0']
             ),
             Node(
                 package='tf2_ros',
                 executable='static_transform_publisher',
-                name='tf2_ros2', 
                 arguments=['-1', '-1', '1', '0', '0', '0', 'palm', 'imu2']
             ),
         ]
